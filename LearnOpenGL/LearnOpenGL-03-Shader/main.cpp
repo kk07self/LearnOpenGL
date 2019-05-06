7d38445765d1de8efb1395ed8d02804405f7f837
Kirk <kk.07.self@gmail.com>
2019-05-06 23:17:55 +0800

    多色三角形+gl4.2

--- LearnOpenGL/LearnOpenGL-03-Shader/main.cpp
+++ LearnOpenGL/LearnOpenGL-03-Shader/main.cpp
@@ -27,14 +27,14 @@ const unsigned int SCR_HEIGHT = 600;
 // 顶点着色器的输出值和片段着色器的输入值一样即可满足传递
 // 例如
 // GLSL顶点着色器源码
-const char *vertexShaderSource =
-"#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n" // 顶点着色器的颜色输出
-"void main() {\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"   vertexColor = vec4(1.0, 0.5, 0.0, 1.0);\n" // 这里给棕色
-"}\n\0";
+//const char *vertexShaderSource =
+//"#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"out vec4 vertexColor;\n" // 顶点着色器的颜色输出
+//"void main() {\n"
+//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//"   vertexColor = vec4(1.0, 0.5, 0.0, 1.0);\n" // 这里给棕色
+//"}\n\0";
 
 // GLSL片段着色器，计算像素最后的颜色输出
 //const char *fragmentShaderSource =
@@ -48,15 +48,37 @@ const char *vertexShaderSource =
 
 #pragma mark - Uniform
 // uniform是全局的，CPU向GPU发送数据
+//const char *fragmentShaderSource =
+//"#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"uniform vec4 ourColor;\n" // 接受外部的数据
+//"void main() {\n"
+//"   FragColor = ourColor;\n" // 将外面传来的文理颜色进行赋值
+//"}\n\0";
+
+
+#pragma mark - 顶点着色器---片段着色器---数据源自顶点数组对象
+// 例如
+// GLSL顶点着色器源码
+const char *vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"   // 从0的位置区
+"layout (location = 1) in vec3 aColor;\n" // 从1的位置取
+"out vec3 vertexColor;\n" // 顶点着色器的颜色输出
+"void main() {\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   vertexColor = aColor;\n" // 这里给棕色
+"}\n\0";
+
+// GLSL片段着色器，计算像素最后的颜色输出
 const char *fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n" // 接受外部的数据
+"in  vec3 vertexColor;\n" // 接受顶点着色器传递的值
 "void main() {\n"
-"   FragColor = ourColor;\n" // 将外面传来的文理颜色进行赋值
+//"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+"   FragColor = vec4(vertexColor, 1.0);\n" // 将顶点着色器传来的文理颜色进行赋值
 "}\n\0";
-
-
 
 int main(int argc, const char * argv[]) {
     
@@ -150,14 +172,23 @@ int main(int argc, const char * argv[]) {
     //           我们把它设置为GL_FALSE。
     // 第五个参数：步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。
     // 第六个参数：类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
+    // 因为此次点的位置和颜色的位置在一个数组里，颜色紧随位置后面，因此步长为6
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)0);
     // 以顶点位置0作为参数启用顶点属性
     glEnableVertexAttribArray(0);
     
-    // 解绑之前的绑定
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
     
+    // 颜色属性 从1开始, 偏移量是3
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)(3 * sizeof(float)));
+    // 以顶点位置1作为参数启用顶点属性 --- 颜色
+    glEnableVertexAttribArray(1);
+    
+    // 解绑之前的绑定
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+//    glBindVertexArray(0);
+    
+    glUseProgram(shaderProgram);
     // render loop
     while (!glfwWindowShouldClose(window)) {
         // input
@@ -172,16 +203,15 @@ int main(int argc, const char * argv[]) {
         
         // 绘制三角形
         // 使用着色器程序进行渲染
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         
         // update shader uniform
-        float timeValue = glfwGetTime();
-        float redValue = sin(timeValue)/2.0f + 0.5f; // 设置红色随时间进行变化
-        // 拿到ourcolor信息
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        // 进行赋值
-        glUniform4f(vertexColorLocation, redValue, 0.0, 0.0, 1.0);
+//        float timeValue = glfwGetTime();
+//        float redValue = sin(timeValue)/2.0f + 0.5f; // 设置红色随时间进行变化
+//        // 拿到ourcolor信息
+//        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+//        // 进行赋值
+//        glUniform4f(vertexColorLocation, redValue, 0.0, 0.0, 1.0);
         
         // 绘制物体
         // 第一个参数：图元的类型，这里是三角形
@@ -203,6 +233,28 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+/**
+ 三角形：初始化顶点数据，并绑定、填充到缓冲区 ---- 附带颜色数据
+ 
+ @param VAO 顶点数据对象
+ @param VBO 顶点缓冲对象
+ */
+void bindTriangle(unsigned int *VAO, unsigned int *VBO) {
+    float vertices[] = {
+        // 位置                 // 颜色
+        0.5f,  -0.5f, 0.0f,    1.0, 0.0, 0.0,// 右下 红
+        -0.5f, -0.5f, 0.0f,    0.0, 1.0, 0.0,// 左下 绿
+        0.0f,  0.5f,  0.0f,    0.0, 0.0, 1.0// 上上 蓝
+    };
+    glGenVertexArrays(1, VAO);
+    glGenBuffers(1, VBO);
+
+    // 绑定顶点数组对象、绑定和设置顶点缓冲对象、再设置顶点解析
+    // 复制顶点数组到缓冲中供OpenGL使用
+    glBindVertexArray(*VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
 
 /**
  三角形：初始化顶点数据，并绑定、填充到缓冲区
@@ -210,21 +262,21 @@ int main(int argc, const char * argv[]) {
  @param VAO 顶点数据对象
  @param VBO 顶点缓冲对象
  */
-void bindTriangle(unsigned int *VAO, unsigned int *VBO) {
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // 左
-        0.5f, -0.5f, 0.0f, // 右
-        0.0f,  0.5f, 0.0f, // 上
-    };
-    glGenVertexArrays(1, VAO);
-    glGenBuffers(1, VBO);
-    
-    // 绑定顶点数组对象、绑定和设置顶点缓冲对象、再设置顶点解析
-    // 复制顶点数组到缓冲中供OpenGL使用
-    glBindVertexArray(*VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-}
+//void bindTriangle(unsigned int *VAO, unsigned int *VBO) {
+//    float vertices[] = {
+//        -0.5f, -0.5f, 0.0f, // 左
+//        0.5f, -0.5f, 0.0f, // 右
+//        0.0f,  0.5f, 0.0f, // 上
+//    };
+//    glGenVertexArrays(1, VAO);
+//    glGenBuffers(1, VBO);
+//
+//    // 绑定顶点数组对象、绑定和设置顶点缓冲对象、再设置顶点解析
+//    // 复制顶点数组到缓冲中供OpenGL使用
+//    glBindVertexArray(*VAO);
+//    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//}
 
 
 // 监听是否点击了退出按键
