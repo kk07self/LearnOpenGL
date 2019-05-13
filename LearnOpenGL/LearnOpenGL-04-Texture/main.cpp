6e9c02c40d291c00908867ad292d73e58de2255b
陈康 <kk.07.self@gmail.com>
2019-05-13 14:07:24 +0800

    仓库语言设置成C++

---
+++ LearnOpenGL/LearnOpenGL-04-Texture/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL-04-Texture
+//
+//  Created by tutu on 2019/5/13.
+//  Copyright © 2019 KK. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
